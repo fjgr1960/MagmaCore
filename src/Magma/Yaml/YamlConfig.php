93746480b3ac0dcdbe8f8b330788d6415a616e98
fjgr1960 <paco@gimenobarrio.es>
2021-03-10 12:55:04 +0100

    added Yaml component requiring Symfony\Yaml composer depedency

---
+++ src/Magma/Yaml/YamlConfig.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magma\Yaml;
+
+use Symfony\Component\Yaml\Yaml;
+use Magma\Base\Exception\BaseException;
+use Symfony\Component\Yaml\Exception\ParseException;
+
+class YamlConfig
+{
+  /**
+   * isFileExists function
+   *
+   * @param string $filename
+   * @return void
+   * @throws BaseException
+   */
+  private function isFileExists(string $filename): void
+  {
+    if (!file_exists($filename)) {
+      throw new BaseException("$filename does not exist!");
+    }
+  }
+
+  /**
+   * getYaml function
+   *
+   * @param string $yamlFile
+   * @return mixed
+   */
+  public function getYaml(string $yamlFile): mixed
+  {
+    foreach (glob(CONFIG_PATH . DS .'*.yaml') as $file) {
+      $this->isFileExists($file);
+      $parts = parse_url($file);
+      $path = $parts['path'];
+      if (strpos($path, $yamlFile)) {
+        return Yaml::parseFile($yamlFile);
+      }
+    }
+  }
+
+  /**
+   * file function
+   *
+   * @param string $yamlFile
+   * @return mixed
+   */
+  public static function file(string $yamlFile): mixed
+  {
+    return (new YamlConfig)->getYaml($yamlFile);
+  }
+
+}
